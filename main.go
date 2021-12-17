b57b5f830d08077be37bb6c2c3ca57f641b30a00
Pyshios <58892350+Pyshios@users.noreply.github.com>
2021-12-17 09:03:53 +0200

    Update and rename app.py to main.go

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", Server)
+	http.ListenAndServe(":8888", nil)
+}
+
+func Server(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Unico APP %s", r.URL.Path[1:])
+
+	//fmt.Fprintln(nil)
+}
